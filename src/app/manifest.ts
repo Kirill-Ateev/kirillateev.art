2c635ecbeb758927de053d7298f29bfea7de8a74
Kirill Ateev <g810bAKO@yandex.ru>
2024-09-08 16:39:25 +0300

    feat: add manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Kirill Ateev - Artist',
+    short_name: 'Kirill Ateev - Artist',
+    description:
+      'Welcome to the official website of Kirill Ateev, a contemporary artist known for his unique and captivating artworks. Explore his portfolio, exhibitions, community, and more.',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  };
+}
