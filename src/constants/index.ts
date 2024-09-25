9f2b3939699c3de867aaf50a6a6438c9ac9d8c0d
Kirill Ateev <g810bAKO@yandex.ru>
2024-09-25 19:31:24 +0300

    chore: comment basepaths

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const isDevelopment =
   !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-export const basePath = isDevelopment ? '' : '/personal-landing';
+export const basePath = isDevelopment ? '' : '';
