552a5c10848c066b71187d988508926b5d1e6ae6
Kirill Ateev <g810bAKO@yandex.ru>
2025-10-08 17:06:47 +0300

    feat: add from suprematism message

--- src/constants/text.ts
+++ src/constants/text.ts
@@ -3,6 +3,23 @@ export const messagesList: {
     [lang: string]: { key: string; title: string; text: string };
   };
 } = {
+  [`from-suprematism-to-kinimalism`]: {
+    ru: {
+      key: `from-suprematism-to-kinimalism`,
+      title: `От супрематизма к кинимализму`,
+      text: `Если супрематизм изъявляет собой наивысшую концептуальную форму изобразительного искусства, то кинимализм возвращается к изначальной беспредметной форме, отделяясь от материалистической основы, декларируя себя как полностью автономное идеалистическое искусство в себе, представляясь в виде точного математического относительного описания. Другими словами, это выражение основы работы, то что ее порождает. Подобный подход открывает новые возможности не только для восприятия работ и хранения, но и для соответствующей контексту интерпретации. 
+
+Материальные супрематические работы вынуждены существовать внутри формы: холста, красок, размеров, что фактически делает их вписанными в ограничения материальной формы, от того наследуя ее предметность и хрупкость. Материальные формы, в силу своего краткосрочного существования, также не имеют возможности изображать движение. Однако движение можно выразить математически, в качестве самой работы и интерпретировать его таким образом в любой момент. Описание движения векторов не требует динамичности самой основы работы как таковой, поэтому, не смотря на статичность математической основы работы, ее интерпретация пребывает в бесконечном движении. 
+`,
+    },
+    en: {
+      key: `from-suprematism-to-kinimalism`,
+      title: `From suprematism to kinimalism`,
+      text: `If suprematism represents the highest conceptual form of visual art, then kinimalism returns to its original non-objective form, separating itself from its materialistic basis and declaring itself to be a completely autonomous idealistic art form in itself, presenting itself as a precise mathematical relative description. In other words, it is an expression of the basis of the work, that which gives rise to it. This approach opens up new possibilities not only for the perception and storage of works, but also for context-appropriate interpretation. 
+
+Material suprematist works are forced to exist within a form: canvas, paints, dimensions, which in fact makes them inscribed in the limitations of material form, thereby inheriting its objectivity and fragility. Material forms, due to their short-term existence, also do not have the ability to depict movement. However, movement can be expressed mathematically, as the work itself, and interpreted in this way at any moment. The description of the movement of vectors does not require the dynamism of the basis of the work itself, so despite the static nature of the mathematical basis of the work, its interpretation remains in perpetual motion.`,
+    },
+  },
   kinimalism: {
     ru: {
       key: `kinimalism`,
