f7ed17e8802cec93d45db41c40f8d25134922dea
Kirill Ateev <g810bAKO@yandex.ru>
2025-12-20 23:45:03 +0300

    chore: correcting texts

--- src/constants/text.ts
+++ src/constants/text.ts
@@ -53,7 +53,7 @@ Thanks to their informational nature, kinimalistic works can be completely extra
       title: `Кинимализм`,
       text: `Кинимализм - творчество движением абсолютной формы. 
 
-Кинимализм признает движение фундаментальной основой произведения и доводит минимализм до изначальной векторной формы, что позволяет описать автономный квант движения.
+Кинимализм признает движение фундаментальной основой произведения и доводит минимализм до изначальной векторной формы, позволяя описать автономный квант движения.
 
 Движение векторов становится идеальной формой. Произведение существует для зрителя в виде точного математического описания. Произведение не имеет ограничений размера и на него не влияет время.`,
     },
@@ -62,7 +62,7 @@ Thanks to their informational nature, kinimalistic works can be completely extra
       title: `Kinimalism`,
       text: `Kinimalism - is creativity through the movement of absolute form.
 
-Kinimalism recognizes movement as the fundamental basis of the work and brings minimalism to the original vector form, which allows one to describe an autonomous quantum of movement.
+Kinimalism recognizes movement as the fundamental basis of a work and reduces minimalism to its original vector form, allowing the description of an autonomous quantum of movement.
 
 The movement of vectors becomes an ideal form. The work exists for the viewer as an exact mathematical description. The work has no size limitations and is not affected by time.`,
     },
